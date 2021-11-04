aaaa26fb5f26a2f70e4c30f6fc7f449b751b4960
Sofia Santos <sofiarsantos31@gmail.com>
2021-11-04 11:14:25 +0000

    update solution for q36

--- website/50q/36/solution.hs
+++ website/50q/36/solution.hs
@@ -1,9 +1,6 @@
 preCrescente :: Ord a => [a] -> [a]
-preCrescente l = preCrescenteAux l [] []
-
-preCrescenteAux :: Ord a => [a] -> [a] -> [a] -> [a]
-preCrescenteAux [] _ l = l
-preCrescenteAux (h:t) curMax maxL
-    | not (null t) && head t >= h = preCrescenteAux t newMax maxL
-    | otherwise = preCrescenteAux t [] (if length newMax > length maxL then newMax else maxL)
-    where newMax = curMax ++ [h]
+preCrescente [] = []
+preCrescente [x] = [x]
+preCrescente (h:s:t)
+    | s >= h = h : preCrescente (s:t)
+    | otherwise = [h]
