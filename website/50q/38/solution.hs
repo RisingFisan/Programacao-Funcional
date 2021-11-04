f2b9caec3edf52200b909062f492a90c48064fbc
Sofia Santos <sofiarsantos31@gmail.com>
2021-11-04 19:43:11 +0000

    add explanation and fix q38

--- website/50q/38/solution.hs
+++ website/50q/38/solution.hs
@@ -1,4 +1,7 @@
 menor :: String -> String -> Bool
 menor _ "" = False
 menor "" _ = True
-menor (h:t) (h':t') = h < h' || menor t t'
+menor (h:t) (h':t')
+    | h < h' = True
+    | h == h' = menor t t'
+    | otherwise = False
