2921be75045a68e74c6c8d59313e4fbca65adc5d
Sofia Santos <sofiarsantos31@gmail.com>
2021-11-06 17:06:34 +0000

    fix website things uwu

--- website/50q/26/example.md
+++ website/50q/26/example.md
@@ -1,4 +1,4 @@
 ```haskell
 > nub [1,2,1,2,3,1,2]
-[1,2,3]
+[1,2,3] -- ou [3,1,2], se mantivermos as últimas ocorrências
 ```
--- website/50q/26/solution.hs
+++ website/50q/26/solution.hs
@@ -1,14 +1,14 @@
--- Versão sem funções de ordem superior (mantém última ocorrência)
+-- Versão sem funções de ordem superior (mantém primeira ocorrência)
 nub :: Eq a => [a] -> [a]
 nub [] = []
-nub (h:t) = if h `elem` t then nub t else h : nub t
-
--- Versão sem funções de ordem superior (mantém primeira ocorrência)
-nub' :: Eq a => [a] -> [a]
-nub' [] = []
-nub' a = if l `elem` i then nub' i else nub' i ++ [l]
+nub a = if l `elem` i then nub i else nub i ++ [l]
     where i = init a
           l = last a
+
+-- Versão sem funções de ordem superior (mantém última ocorrência)
+nub' :: Eq a => [a] -> [a]
+nub' [] = []
+nub' (h:t) = if h `elem` t then nub' t else h : nub' t
 
 -- Versão com funções de ordem superior
 nub'' :: Eq a => [a] -> [a]
