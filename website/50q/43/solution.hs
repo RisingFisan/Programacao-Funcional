d754112f262c68601badd115a9ddc987e07c9b36
Sofia Santos <sofiarsantos31@gmail.com>
2021-11-06 18:01:57 +0000

    added to q43

--- website/50q/43/solution.hs
+++ website/50q/43/solution.hs
@@ -1,4 +1,9 @@
 constroiMSet :: Ord a => [a] -> [(a,Int)]
 constroiMSet [] = []
 constroiMSet (l:ls) = insereMSet l (constroiMSet ls)
--- Função `insereMSet` definida na questão 39
+-- Função `insereMSet` definida na questão 41
+
+-- em alternativa, esta solução não cria a lista "ao contrário", apesar de estar correto das duas formas:
+constroiMSet' :: Ord a => [a] -> [(a,Int)]
+constroiMSet' [] = []
+constroiMSet' l = insereMSet (last l) (constroiMSet' (init l))
