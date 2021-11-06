c417fb9bab9b7aa58955e8aeace68a7bc8c9bd00
Sofia Santos <sofiarsantos31@gmail.com>
2021-11-06 16:42:49 +0000

    fix q42

--- website/50q/42/solution.hs
+++ website/50q/42/solution.hs
@@ -1,3 +1,5 @@
 removeMSet :: Eq a => a -> [(a,Int)] -> [(a,Int)]
 removeMSet x [] = []
-removeMSet x ((a,n):xs) = if x == a then xs else (a,n) : removeMSet x xs
+removeMSet x ((a,n):xs) 
+    | x == a = if n > 1 then (a, n-1) : xs else xs
+    | otherwise = (a,n) : removeMSet x xs
